5f2585ec6b7e5a9a29baf33a490e49b807c9ba0b
amatlock-cctc <38077611+amatlock-cctc@users.noreply.github.com>
2020-01-15 10:10:39 -0600

    Update Main.java

--- com/company/Main.java
+++ com/company/Main.java
@@ -8,22 +8,7 @@ public class Main {
 
     public static void main(String[] args)
     {
-        // Create the world.
-        world.setVisible(true);
-        world.setTitle("Pong: A brave new world!");
-        world.setSize(480, 480);
-        world.setResizable(false);
-
-        // The land
-        JPanel land = new JPanel();
-        land.setBackground(Color.GREEN);
-
-        Ball ball = new Ball();
-
-        land.add(ball);
-
-        world.setContentPane(land);
-        world.repaint();
+        System.out.println("Hello World");
     }
 
 }

56ad2f409ddd7854bf71f90af0b8c2603168c101
CordusBailey02 <cordusbailey02@gmail.com>
2020-01-22 10:54:32 -0600

    Changed the code to say my first and last name.

--- src/com/caddocareer/Main.java
+++ src/com/caddocareer/Main.java
@@ -4,7 +4,7 @@ public class Main extends ConsoleProgram {
 
     public static void main(String[] args)
     {
-        System.out.println("Hello World");
+        System.out.println("Cordus Bailey");
     }
 
 }

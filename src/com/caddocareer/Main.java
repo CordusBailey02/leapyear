c35b7ea47baca647966a3d110a4fe909bd47d430
CordusBailey02 <cordusbailey02@gmail.com>
2020-01-30 11:30:50 -0600

    Changed the output of the run method

--- src/com/caddocareer/Main.java
+++ src/com/caddocareer/Main.java
@@ -4,7 +4,9 @@ public class Main extends ConsoleProgram {
 
     public static void main(String[] args)
     {
-        System.out.println("Cordus Bailey");
+        System.out.println("Goon Pod");
+
+        //Purpose determined later
     }
 
 }
